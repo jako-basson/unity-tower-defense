fcc886af6ab527a31ebd578d316a4126a6917c77
jako-basson <jakobasson@gmail.com>
2017-11-15 18:22:23 -0600

    Added some core game elements

---
+++ Unity Tower Defense/Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 70f;
+
+    private Transform _target;
+    public GameObject impactEffect;
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
+
+	void Update () {
+        if (_target == null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = _target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceThisFrame) {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+	}
+
+    void HitTarget() {
+        var effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 2f);
+        Destroy(gameObject);
+        Destroy(_target.gameObject);
+    }
+}
---
+++ Unity Tower Defense/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = WaypointsController.points[wavepointIndex];    
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f) {
+            SetNextWayPoint();
+        }
+    }
+
+    void SetNextWayPoint() {
+        if (wavepointIndex >= WaypointsController.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = WaypointsController.points[wavepointIndex];
+    }
+}
